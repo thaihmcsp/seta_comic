c522e7e9616c7fa9d513adcfa111b1db3576b0b0
thai <thaihmcsp@gmail.com>
2025-07-20 17:59:24 +0700

    api get comic comment

--- app/routes/commentRoute.py
+++ app/routes/commentRoute.py
@@ -1,13 +1,20 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, Query
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.db.deps import get_session
+from ..services.commentService import getCommentOfComic
 
 router = APIRouter(
-    prefix="/comment",
-    tags=["comment"],
-    # dependencies=[Depends(get_token_header)],
-    # responses={404: {"description": "Not found"}},
+  prefix="/comment",
+  tags=["comment"],
+  # dependencies=[Depends(get_token_header)],
+  # responses={404: {"description": "Not found"}},
 )
 
 
-@router.get("/comic/:comic_id")
-async def listComment():
-    return [{"user_id": 1, "comic_id": 1, "chapter_id": 1, "content": "test"}]
+@router.get("/comic/{comic_id}")
+async def listComment(
+  comic_id: int,
+  page: int = Query(1, ge=1),
+  session: AsyncSession = Depends(get_session),
+):
+  return await getCommentOfComic(comic_id, page, session)
---
+++ app/services/commentService.py
@@ -0,0 +1,19 @@
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+async def getCommentOfComic (comic_id: int, page: int, session: AsyncSession):
+  sql = text(
+      """
+    SELECT c.*, u.username
+    FROM comments c
+    INNER JOIN users u ON u.id = c.user_id
+    WHERE c.comic_id = :comic_id and c.chapter_id IS NULL
+    ORDER BY c.created_at
+    OFFSET 20 * (:page - 1) LIMIT 20
+  """
+  )
+
+  result = await session.execute(sql, {"page": page, "comic_id": comic_id})
+  rows = result.fetchall()
+  return [dict(row._mapping) for row in rows]
