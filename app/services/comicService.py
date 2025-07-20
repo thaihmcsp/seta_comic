2cf3333942cf0f2a7cbd7bbcab46301854fbc8e2
thai <thaihmcsp@gmail.com>
2025-07-20 17:27:40 +0700

    show category when get comic

--- app/services/comicService.py
+++ app/services/comicService.py
@@ -4,9 +4,16 @@ from sqlalchemy.ext.asyncio import AsyncSession
 async def getComicById (comic_id: int, session: AsyncSession):
   sql = text(
     """
-      SELECT c.*, u.username AS author_name, u.email AS author_email
+      SELECT c.*, u.username AS author_name, u.email AS author_email, categories
       FROM comics c
       INNER JOIN users u ON c.author_id = u.id
+      LEFT JOIN (
+      	SELECT ca.comic_id, ARRAY_AGG(cc.name) AS categories
+      	FROM comic_categories ca
+      	INNER JOIN categories cc ON cc.id = ca.category_id
+      	WHERE ca.comic_id = :comic_id
+      	GROUP BY ca.comic_id
+      ) l on l.comic_id = c.id
       WHERE c.id = :comic_id
     """
   )
@@ -17,9 +24,15 @@ async def getComicById (comic_id: int, session: AsyncSession):
 async def getComicOfAuthor (author_id: int, page: int, session: AsyncSession):
   sql = text(
     """
-      SELECT c.*, u.username AS author_name, u.email AS author_email
+      SELECT c.*, u.username AS author_name, u.email AS author_email, categories
       FROM comics c
       INNER JOIN users u ON c.author_id = u.id
+      LEFT JOIN (
+      	SELECT ca.comic_id, ARRAY_AGG(cc.name) AS categories
+      	FROM comic_categories ca
+      	INNER JOIN categories cc ON cc.id = ca.category_id
+      	GROUP BY ca.comic_id
+      ) l on l.comic_id = c.id
       WHERE u.id = :author_id
       ORDER BY c.title
       OFFSET 20 * (:page - 1) LIMIT 20
@@ -31,9 +44,15 @@ async def getComicOfAuthor (author_id: int, page: int, session: AsyncSession):
 
 async def searchComic (title: str | None, page: int, session: AsyncSession):
   titleCondition = "WHERE c.title ILIKE :title" if title else ""
-  sqlQuery = f'''SELECT c.*, u.username AS author_name, u.email AS author_email
+  sqlQuery = f'''SELECT c.*, u.username AS author_name, u.email AS author_email, categories
     FROM comics c
     INNER JOIN users u ON c.author_id = u.id
+    LEFT JOIN (
+      SELECT ca.comic_id, ARRAY_AGG(cc.name) AS categories
+      FROM comic_categories ca
+      INNER JOIN categories cc ON cc.id = ca.category_id
+      GROUP BY ca.comic_id
+    ) l on l.comic_id = c.id
     {titleCondition}
     ORDER BY c.title
     OFFSET 20 * (:page - 1) LIMIT 20
