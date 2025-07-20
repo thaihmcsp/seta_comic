79e1e69fe5ab5f2e08bcd276338904dfb44fd167
thai <thaihmcsp@gmail.com>
2025-07-20 15:06:40 +0700

    add api get comic by authorID

--- app/routes/comicRoute.py
+++ app/routes/comicRoute.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.deps import get_session
-from ..services.comicService import getComicById
+from ..services.comicService import getComicById, getComicOfAuthor
 
 router = APIRouter(
   prefix="/comic",
@@ -13,3 +13,11 @@ router = APIRouter(
 @router.get('/{comic_id}')
 async def getComicDataById(comic_id: int, session: AsyncSession = Depends(get_session)):
   return await getComicById(comic_id, session)
+
+@router.get('/author/{author_id}')
+async def getComicListByAuthorId(
+    author_id: int,
+    page: int = Query(1, ge=1),
+    session: AsyncSession = Depends(get_session)
+  ):
+    return await getComicOfAuthor(author_id, page, session)
--- app/services/comicService.py
+++ app/services/comicService.py
@@ -13,3 +13,18 @@ async def getComicById (comic_id: int, session: AsyncSession):
   result = await session.execute(sql, {"comic_id": comic_id})
   rows = result.fetchall()
   return [dict(row._mapping) for row in rows]
+
+async def getComicOfAuthor (author_id: int, page: int, session: AsyncSession):
+  sql = text(
+    """
+      SELECT c.*, u.username AS author_name, u.email AS author_email
+      FROM comics c
+      INNER JOIN users u ON c.author_id = u.id
+      WHERE u.id = :author_id
+      ORDER BY c.title
+      OFFSET 20 * (:page - 1) LIMIT 20
+    """
+  )
+  result = await session.execute(sql, {"author_id": author_id, "page": page})
+  rows = result.fetchall()
+  return [dict(row._mapping) for row in rows]
