00eacdec5fdecd7be8ddd19c1b125e2ae681a7c6
thaihmcsp <66521122+thaihmcsp@users.noreply.github.com>
2025-07-28 22:50:46 +0700

    Thai/no auth api (#1)
    * update get user by id api
    
    * search author  api
    
    * update get comic by id api
    
    * add api get comic by authorID
    
    * add api search comic by name
    
    * add api get list chapter
    
    * add page and chapter api
    
    * show category when get comic
    
    * add get category API
    
    * add get comic by category api
    
    * api get comic comment
    
    * api get chapter comment
    
    ---------
    
    Co-authored-by: thai <thaihmcsp@gmail.com>

--- app/routes/pageRoute.py
+++ app/routes/pageRoute.py
@@ -1,12 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, Query
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.db.deps import get_session
+from ..services.pageService import getPagesOfOneChapter, getOnePage
 
 router = APIRouter(
-    prefix="/page",
-    tags=["page"],
-    # dependencies=[Depends(get_token_header)],
-    # responses={404: {"description": "Not found"}},
+  prefix="/page",
+  tags=["page"],
+  # dependencies=[Depends(get_token_header)],
+  # responses={404: {"description": "Not found"}},
 )
 
 @router.get("/chapter/:chapter_id")
-async def getListPageOfChapter():
-    return [{"chapter_id": 1, "image_url": "....", "page_index": 1}]
+async def getListPageOfChapter(
+    chapter_id: int,
+    session: AsyncSession = Depends(get_session)
+  ):
+  return await getPagesOfOneChapter(chapter_id, session)
+
+@router.get("/:page_id")
+async def getPageById(
+    page_id: int,
+    session: AsyncSession = Depends(get_session)
+  ):
+  return await getOnePage(page_id, session)
--- app/routes/userRoute.py
+++ app/routes/userRoute.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from sqlalchemy.ext.asyncio import AsyncSession
-from ..services.userService import listUser, getUserByUserId
+from ..services.userService import listUser, getUserByUserId, searchAuthorByText
 from app.db.deps import get_session
 
 router = APIRouter(
@@ -10,15 +10,24 @@ router = APIRouter(
   # responses={404: {"description": "Not found"}},
 )
 
+# need check auth
 @router.get('/me')
 async def getMyInfo():
   return {"username": "test", "email": "test@gmail.com"}
 
+# need check role admin
 @router.get('/')
 async def getlistUser(session: AsyncSession = Depends(get_session)):
   return await listUser(session)
 
+@router.get('/authors')
+async def searchAuthors(
+    page: int = Query(1, ge=1),
+    name: str | None = None,
+    session: AsyncSession = Depends(get_session)
+  ):
+  return await searchAuthorByText(page, name, session)
+
 @router.get('/{user_id}')
 async def getUserById(user_id: int, session: AsyncSession = Depends(get_session)):
-  print(23, user_id)
   return await getUserByUserId(user_id, session)
