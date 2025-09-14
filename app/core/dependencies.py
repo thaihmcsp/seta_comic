7132233f97558e966d2649f1d79fe0079ed1643d
thaihmcsp <66521122+thaihmcsp@users.noreply.github.com>
2025-09-14 14:55:09 +0700

    comic add and update api (#3)
    Co-authored-by: thai <thaihmcsp@gmail.com>

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -11,6 +11,9 @@ from app.services.authService import decode_access_token
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 async def get_current_user(token: str = Depends(oauth2_scheme),session: AsyncSession = Depends(get_session),) -> User:
+    if not token:
+        raise HTTPException(401, "Missing bearer token")
+
     payload = decode_access_token(token)
     if not payload:
         raise HTTPException(status_code=401, detail="Invalid token")
---
+++ app/schemas/comic.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+class UpdateComicRequest(BaseModel):
+    title: Optional[str] = Field(None, title="Title of the comic")
+    description: Optional[str] = Field(None, description="Description of the comic")
+    cover_url: Optional[str] = Field(None, description="Cover URL of the comic")
+    is_published: Optional[bool] = Field(None, description="Publication status of the comic")
+    is_premium: Optional[bool] = Field(None, description="Premium status of the comic")
+    slug: Optional[str] = Field(None, description="Slug for the comic")
+
+
+class CreateComicRequest(BaseModel):
+    title: str
+    description: Optional[str]
+    cover_url: Optional[str]
